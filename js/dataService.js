a69b27d03d00ba8f5f8016699a8d07a9898e528b
Squire Johnson <squire.a.johnson@gmail.com>
2015-09-07 22:51:35 -0600

    finished

--- js/dataService.js
+++ js/dataService.js
@@ -1,17 +1,17 @@
 var app = angular.module('quoteBook');
 app.service('myService', function() {
 var quotes = [
+    
   ]
 	this.getData = function () {
-		quotes = JSON.parse(localStorage["quotes"]);
-		console.log(quotes)
+		quotes = JSON.parse(localStorage["quotes"])
 		return quotes;
 	}//getData
 
 	this.addData = function (newObj) {
 	/* addData takes in a data object, verifies that data object has the proper keys (just text and author), then adds that object to the end of the quotes array*/
 		if((newObj.author)&&(newObj.quote)) {
-			quotes.push({'text': newObj.quote, 'author': newObj.author})
+			quotes.unshift({'text': newObj.quote, 'author': newObj.author})
 		}
 			localStorage["quotes"] = JSON.stringify(quotes);
 			console.log(newObj);
@@ -33,6 +33,5 @@ var quotes = [
 	}
 
 ;
-
 
 }); //end
