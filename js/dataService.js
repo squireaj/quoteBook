6f1964d8659a517b458923eb465164fc7ad97ced
Squire Johnson <squire.a.johnson@gmail.com>
2015-09-07 22:43:33 -0600

    local storage working

--- js/dataService.js
+++ js/dataService.js
@@ -1,7 +1,10 @@
 var app = angular.module('quoteBook');
 app.service('myService', function() {
-
+var quotes = [
+  ]
 	this.getData = function () {
+		quotes = JSON.parse(localStorage["quotes"]);
+		console.log(quotes)
 		return quotes;
 	}//getData
 
@@ -10,8 +13,9 @@ app.service('myService', function() {
 		if((newObj.author)&&(newObj.quote)) {
 			quotes.push({'text': newObj.quote, 'author': newObj.author})
 		}
-
+			localStorage["quotes"] = JSON.stringify(quotes);
 			console.log(newObj);
+
 	} //addData
 
 	this.removeData = function (prop) {
@@ -28,15 +32,7 @@ app.service('myService', function() {
 		thing = !thing
 	}
 
-var quotes = [
-    { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
-    { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
-    { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
-    { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
-    { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
-    { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
-    { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
-  ];
+;
 
 
 }); //end
